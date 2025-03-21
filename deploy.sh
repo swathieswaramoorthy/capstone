6519b36c27f0f5dc32169e604b35185380214bd1
swathieswaramoorthy <swathie.22cse@kongu.edu>
2025-03-21 13:40:56 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
-    docker-compose up -d
+    docker login -u swathie1701 -p dckr_pat_s3UZ51gMHkSwHUTXokG1ORWxst8
+    docker tag test1 swathie1701/devops
+    docker push swathie1701/devops
     
